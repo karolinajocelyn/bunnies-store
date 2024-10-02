91cff3cdce30007d1cab19ef0909896de36f7986
karolinajocelyn <karolina.jocelyn@gmail.com>
2024-10-02 11:35:24 +0700

    new look and landing page of bunnies store

--- main/forms.py
+++ main/forms.py
@@ -1,8 +1,63 @@
+from django import forms
 from django.forms import ModelForm
-from main.models import Product
+from .models import Product
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.models import User
 
 class ProductForm(ModelForm):
     class Meta:
-        model = Product
-        fields = ["artist_name", "album_title", "price", "description"]
+        model = Product  # This is the model the form is based on
+        fields = ['artist_name', 'album_title', 'price', 'description']  # Include the fields you want to display in the form
 
+        # Optional: Customize widgets for form fields
+        widgets = {
+            'artist_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter the artist name',
+            }),
+            'album_title': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter the album title',
+            }),
+            'price': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter the price',
+                'min': 0
+            }),
+            'description': forms.Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter the album description',
+                'rows': 3,
+            }),
+        }
+        
+class CustomAuthenticationForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'w-full max-w-lg px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-pink-500 focus:border-pink-500 text-center',
+        'placeholder': 'Username'
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'w-full max-w-lg px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-pink-500 focus:border-pink-500 text-center',
+        'placeholder': 'Password'
+    }))
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2']
+        
+        # Define widgets with custom classes
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'w-full max-w-lg px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-pink-500 focus:border-pink-500 text-center',
+                'placeholder': 'Username'
+            }),
+            'password1': forms.PasswordInput(attrs={
+                'class': 'w-full max-w-lg px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-pink-500 focus:border-pink-500 text-center',
+                'placeholder': 'Password'
+            }),
+            'password2': forms.PasswordInput(attrs={
+                'class': 'w-full max-w-lg px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-pink-500 focus:border-pink-500 text-center',
+                'placeholder': 'Password confirmation'
+            }),
+        }
--- main/views.py
+++ main/views.py
@@ -1,15 +1,20 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from django.shortcuts import render, redirect   # Tambahkan import redirect di baris ini
-from main.forms import ProductForm
+from django.shortcuts import render, redirect, reverse   # Tambahkan import redirect di baris ini
+from main.forms import ProductForm, CustomAuthenticationForm, CustomUserCreationForm
 from main.models import Product
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.core import serializers
 from django.contrib.auth.decorators import login_required
 import datetime
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+
+def show_landing_page(request):
+    if request.user.is_authenticated:
+        return redirect('main:show_main')
+    return render(request, 'landing_page.html')
 
 @login_required(login_url='/login')
 def show_main(request):
@@ -35,6 +40,29 @@ def create_product(request):
     context = {'form': form}
     return render(request, "create_product.html", context)
 
+def edit_product(request, id):
+    # Get product by id
+    product = Product.objects.get(pk = id)
+    
+    # Set mood entry sebagai instance dari form
+    form = ProductForm(request.POST or None, instance = product)
+
+    if form.is_valid() and request.method == "POST":
+        # Simpan form dan kembali ke halaman awal
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "edit_product.html", context)
+
+def delete_product(request, id):
+    # Get mood berdasarkan id
+    product = Product.objects.get(pk = id)
+    # Hapus mood
+    product.delete()
+    # Kembali ke halaman awal
+    return HttpResponseRedirect(reverse('main:show_main'))
+
 def show_xml(request):
     data = Product.objects.all()
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
@@ -52,35 +80,33 @@ def show_json_by_id(request, id):
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def register(request):
-    form = UserCreationForm()
-
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, 'Your account has been successfully created!')
             return redirect('main:login')
-    context = {'form':form}
+    else:
+        form = CustomUserCreationForm()
+
+    context = {'form': form}
     return render(request, 'register.html', context)
 
-def login_user(request):
-   if request.method == 'POST':
-      form = AuthenticationForm(data=request.POST)
 
-      if form.is_valid():
+def login_user(request):
+    if request.method == 'POST':
+        form = CustomAuthenticationForm(data=request.POST)  # Use the custom form
+        if form.is_valid():
             user = form.get_user()
             login(request, user)
-            response = HttpResponseRedirect(reverse("main:show_main"))
-            response.set_cookie('last_login', str(datetime.datetime.now()))
-            return response
+            return redirect('main:show_main')
+    else:
+        form = CustomAuthenticationForm()  # Use the custom form
         
-   else:
-      form = AuthenticationForm(request)
-   context = {'form': form}
-   return render(request, 'login.html', context)
+    return render(request, 'login.html', {'form': form})
 
 def logout_user(request):
     logout(request)
-    response = HttpResponseRedirect(reverse('main:login'))
+    response = HttpResponseRedirect(reverse('main:show_landing_page'))
     response.delete_cookie('last_login')
-    return redirect('main:login')
+    return redirect('main:show_landing_page')
