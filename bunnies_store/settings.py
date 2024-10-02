82ea2d906cc7ed2e166d278c4dfa5a86989b69ed
karolinajocelyn <karolina.jocelyn@gmail.com>
2024-10-02 11:40:27 +0700

    debug = False

--- bunnies_store/settings.py
+++ bunnies_store/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9f0rj#cq-3z&nyh=)gcp-zh^5@nv(kbr8-9s$9@f_^na$fqxe8
 
 # SECURITY WARNING: don't run with debug turned on in production!
 PRODUCTION = os.getenv("PRODUCTION", False)
-DEBUG = not PRODUCTION
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "karolina-jocelyn-bunniesstore.pbp.cs.ui.ac.id"]
 
